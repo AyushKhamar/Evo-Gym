311f4fe1f1039ac1f93cacc588f6a0487ade9992
AyushKhamar <ayushkhamar8@gmail.com>
2024-07-30 00:30:56 +0530

    feat: add our classes component

---
+++ src/scenes/ourclasses/index.tsx
@@ -0,0 +1,92 @@
+import { HomePageProps } from "../home";
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
+import { motion } from "framer-motion";
+import { ClassType } from "@/shared/types";
+import { SelectedPage } from "@/shared/types";
+import HeadingText from "@/shared/HeadingText";
+import Class from "./Class";
+const classes: Array<ClassType> = [
+  {
+    name: "Weight Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image1,
+  },
+  {
+    name: "Yoga Classes",
+    image: image2,
+  },
+  {
+    name: "Ab Core Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image3,
+  },
+  {
+    name: "Adventure Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image4,
+  },
+  {
+    name: "Fitness Classes",
+    image: image5,
+  },
+  {
+    name: "Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image6,
+  },
+];
+export const OurClasses = ({ setSelectedPage }: HomePageProps) => {
+  return (
+    <section className="w-full bg-primary-100 py-40 " id="ourclasses">
+      <motion.div
+        onViewportEnter={() => {
+          setSelectedPage(SelectedPage.OurClasses);
+        }}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HeadingText>Our Classes</HeadingText>
+            <p className="py-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum est
+              error voluptas unde quos ipsum temporibus non ea optio sequi porro
+              distinctio voluptatem excepturi officiis a, fugiat, quia eum quod.
+            </p>
+          </div>
+        </motion.div>
+        <div className="mt-10 h-[353px] overflow-x-auto overflow-y-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classes.map((item, index) => {
+              return (
+                <Class
+                  image={item.image}
+                  description={item.description}
+                  name={item.name}
+                  key={index}
+                ></Class>
+              );
+            })}
+          </ul>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
